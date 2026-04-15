46996d8a1375b47e537c4b29c8d6cb99a9e2da95
Scott Fuller <shrgf1234@gmail.com>
2026-04-15 12:36:38 +0100

    footer

---
+++ scottfuller.dev/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="max-w-screen-lg w-full text-center py-6 opacity-60">
+            <p>&copy; 2026</p>
+        </div>
+    )
+}
+
+export default Footer
