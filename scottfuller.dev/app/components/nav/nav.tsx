8f6c1b73813d51ac0fe88393d3c6a9fa871051b6
Scott Fuller <shrgf1234@gmail.com>
2026-04-15 12:34:59 +0100

    .

---
+++ scottfuller.dev/app/components/nav/nav.tsx
@@ -0,0 +1,17 @@
+const Logo = ({className}: {className?: string}) => {
+    return (
+        <img alt="logo" src="/logo.png" className={'w-20 ${className}'} />
+    )
+}
+
+const NavBar = () => {
+    return (
+        <nav className = "navbar">
+            <div className="w-full flex justify-center items-center">
+                <Logo />
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
